8152fd7cdc07b0d579430fb12f708c62e97b73f5
Norrec <sergej.morokin@googlemail.com>
2016-03-31 18:56:09 +0200

    Update consumers.py
    added GroupConsumer

--- channels/consumers.py
+++ channels/consumers.py
@@ -10,12 +10,36 @@ class Consumer:
     
     def disconnect(self, message):
         self.channel = None
-        
+
     def receive(self, message):
         call = message.get("call", None)
         if call in valid_calls:
             call_method = getattr(self, call)
             call_method(message)
-        
+
+    def send(self, data):
+        self.channel.send({"text": data})
+
     def echo(self, message):
-        self.channel.send({"text": message.get("text", None)})
+        data = message.get("text", None)
+        self.send(data)
+
+
+class GroupConsumer(Consumer):
+    group_name
+    
+    @property
+    def channel(self):
+        return Group(group_name)
+    
+    def connect(self, message):
+        self.channel.add(message.reply_channel)
+    
+    def disconnect(self, message):
+        self.channel.discard(message.reply_channel)
+        
+        
+        
+    
+    
+    
